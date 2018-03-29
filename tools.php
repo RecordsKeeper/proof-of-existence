12ff981f89c9107f4da19dc5a2e36cd59860ec2a
adarsha jha <adarshajha@live.com>
2018-03-29 11:52:13 +0530

    footer links changed

--- tools.php
+++ tools.php
@@ -9,6 +9,10 @@ include_once 'include/nav.php';
     bottom: -30px;
     font-size: 10px;
 }
+#tool {
+    font-size: 12px;
+}
+
 </style>
 <script src="js/index.js" type="text/javascript"/></script>
 <html>
@@ -29,21 +33,22 @@ include_once 'include/nav.php';
             
             <div class="col-md-12">
                 
-                  <p>
+                  
 
                         <ul id="tool">
 
 
                               <li><a href="http://explorer.recordskeeper.co/" target="_blank">Mainnet Explorer</a></li>
                               <li><a href="http://test-explorer.recordskeeper.co/" target="_blank">Testnet Explorer</a></li>
+                              <li><a href="http://wallet.recordskeeper.co/" target="_blank">Wallet</a></li>
                               <li><a href="http://faucet.recordskeeper.co/" target="_blank">Faucet</a></li>
-                              <li><a href="http://stats.recordskeeper.co/" target="_blank">Stats</a></li>
-                              <li><a href="http://miner.recordskeeper.co/" target="_blank">Miner</a></li>
+                              <li><a href="http://stats.recordskeeper.co/" target="_blank">Blockchain Statistics</a></li>
+                              <li><a href="http://miner.recordskeeper.co/" target="_blank">Miner Statistics</a></li>
                               <li><a href="http://airdrop.recordskeeper.co/" target="_blank">Airdrop</a></li>
                               <li><a href="http://demo.recordskeeper.co/" target="_blank">Demo</a></li>
                         </ul>
 
-                  </p>
+                  
                 
             </div>
 
